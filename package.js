bce4a36d1ac382ea2a0bac11bb1d94a3dbf9bd9a
Anthony Astige <AnthonyAstige@users.noreply.github.com>
2015-08-20 16:59:44 -0500

    Version bump for atmosphere

--- package.js
+++ package.js
@@ -3,7 +3,7 @@
 
 Package.describe({
   summary: 'Makes autoforms behave better when offline',
-  version: '0.0.4',
+  version: '0.0.5',
   git: 'https://github.com/AnthonyAstige/ground-autoform',
   name: 'anthonyastige:ground-autoform'
 });
