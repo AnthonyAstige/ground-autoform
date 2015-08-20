9f7a4ba2de8031565c03035a3100627f7f6a7f72
Anthony <Anthony123272@gmail.com>
2015-08-20 14:05:53 -0500

    Package description

--- package.js
+++ package.js
@@ -1,6 +1,13 @@
 'use strict';
 /*global Package*/
 
+Package.describe({
+  summary: 'Makes autoforms behave better when offline',
+  version: '0.0.1',
+  git: 'https://github.com/AnthonyAstige/ground-autoform',
+  name: 'AnthonyAstige:ground-autoform'
+});
+
 Package.onUse(function(api) {
 	api.versionsFrom('1.1.0.3');
 	api.use('standard-app-packages');
